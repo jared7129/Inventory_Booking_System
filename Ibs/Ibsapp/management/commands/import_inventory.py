d37551ea43270969a70d1fa828c6171a8951087d
Jared Joshua <jared@propdata.net>
2021-01-14 08:45:59 +0200

    Update import_inventory.py

--- Ibs/Ibsapp/management/commands/import_inventory.py
+++ Ibs/Ibsapp/management/commands/import_inventory.py
@@ -2,6 +2,7 @@ import csv
 import os
 
 from django.core.management.base import BaseCommand
+from Ibsapp.models import Inventory
 
 
 class Command(BaseCommand):
@@ -19,13 +20,14 @@ class Command(BaseCommand):
             data.append(_data)
         return data
 
-    def handle_noargs(self, *args, **options):
+    def handle(self, *args, **options):
 
         with open('Inventory.csv') as f:
             reader = csv.reader(f)
             members = self.__parse_csv(reader)
         count = 0
         for member in members:
-            count+=Inventory.objects.add(id_no=member['id_no'], title=member['title'], description=['description'], remaining_count=['remaining_count'], expiration_date=['expiration_date'])
+            inserted, err =Inventory.objects.update_or_create(id_no=member['id_no'], title=member['title'], description=member['description'], remaining_count=member['remaining_count'], expiration_date=member['expiration_date'])
+            inserted.save()
         
-        print('Updated {} inventory items'.format(count))
+        print('Updated inventory items')
